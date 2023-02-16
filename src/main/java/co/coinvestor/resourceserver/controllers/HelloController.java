5c44beca907dad9f9a0c9ed3913d23156d6144d6
issac <issac@ip-192-168-0-143.ap-northeast-2.compute.internal>
2023-02-16 10:45:41 +0900

    first commit

---
+++ src/main/java/co/coinvestor/resourceserver/controllers/HelloController.java
@@ -0,0 +1,21 @@
+package co.coinvestor.resourceserver.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Authentication au) {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        System.out.println("oAuth2User = " + au);
+        return "Hello!";
+    }
+}
---
+++ src/test/java/co/coinvestor/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.coinvestor.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
