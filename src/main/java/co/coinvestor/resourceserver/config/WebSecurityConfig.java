de9007ae275950e5fdd68e6efff2d9f4040d3571
issac <issac@ip-192-168-0-143.ap-northeast-2.compute.internal>
2023-02-16 11:12:34 +0900

    error endpoint open

---
+++ src/main/java/co/coinvestor/resourceserver/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package co.coinvestor.resourceserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().mvcMatchers("/error").permitAll().anyRequest().authenticated();
+    }
+}
